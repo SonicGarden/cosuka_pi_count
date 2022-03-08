cf29941bf4cdae1cc551aab84bd8372e9ce995ee
nof <sakape@gmail.com>
2022-03-08 14:15:10 +0900

    refactor: config.nameがシングルクォーテーションになるように

--- lib/generators/cosuka_pi_count/cosuka_pi_count_generator.rb
+++ lib/generators/cosuka_pi_count/cosuka_pi_count_generator.rb
@@ -8,7 +8,7 @@ class CosukaPiCountGenerator < Rails::Generators::Base
     initializer 'cosuka_pi_count.rb' do
       <<~EOF
         CosukaPiCount.configure do |config|
-          config.name = "#{Rails.application.class.module_parent.name.downcase}"
+          config.name = '#{Rails.application.class.module_parent.name.downcase}'
           config.count = -> { User.count }
           config.is_important_private_info = 'true'
           config.kintone_api_token = ENV['KINTONE_COSUKA_PI_COUNT_API_KEY']
