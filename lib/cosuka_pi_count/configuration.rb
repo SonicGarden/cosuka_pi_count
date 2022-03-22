132802e244c1cd848610ce50265965f88df1e173
interu <interu@sonicgarden.jp>
2022-03-22 15:21:07 +0900

    feat: payment_system_nameを追加

--- lib/cosuka_pi_count/configuration.rb
+++ lib/cosuka_pi_count/configuration.rb
@@ -1,6 +1,6 @@
 module CosukaPiCount
   class Configuration
-    attr_accessor :type, :platform, :name, :count, :is_important_private_info, :kintone_api_token
+    attr_accessor :type, :platform, :name, :count, :is_important_private_info, :payment_system_name, :kintone_api_token
 
     def initialize
       self.type = 'standard'
